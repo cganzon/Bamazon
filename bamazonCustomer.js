642c15a8dcb097294693d7dde20226eeb7c6a1b5
cganzon <crganzon@gmail.com>
2019-03-26 17:29:50 -0700

    refactoring

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 // Create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -25,25 +25,20 @@ connection.connect((err, res) => {
     // run the start function after the connection is made to prompt the user
     console.log("\nYou are connected to the Bamazon database");
 
-    // connection.query("SELECT * FROM products", function (err, res) {
-    //     if (err) throw err;
-    //     // Displaying the product info for user to pick from
-    //     console.log("\n");
-    //     console.table(res);
-    //     console.log("\n");
-    // });
     askUser();
 });
 
+// =============================================================
+
 function askUser() {
+    // Displaying the product info for user to pick from
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        // Displaying the product info for user to pick from
         console.log("\n");
         console.table(res);
         console.log("\n");
     });
-    // Using set timeout to fix a bug that caused the table and iquirer prompts to overlap
+    // Using set timeout to fix a bug that caused the table and inquirer prompts to overlap
     setTimeout(function () {
         inquirer.prompt([
             {
@@ -55,41 +50,7 @@ function askUser() {
         ]).then(function (answer) {
             if (answer.purchase === "Make a purchase") {
                 // Asking user which product they want to buy and at what quantity
-                inquirer.prompt([
-                    {
-                        name: "itemID",
-                        type: "input",
-                        message: "Enter the ID for the item you want to buy:"
-                    },
-                    {
-                        name: "quantity",
-                        type: "input",
-                        message: "How many units of the item do you want to buy?"
-                    }
-                ]).then(function (item) {
-                    // Making the purchase request
-                    console.log(`\nThe ID for the item is: ${item.itemID}\nYou want to buy: ${item.quantity} unit(s)`);
-                    connection.query("SELECT * FROM products WHERE item_id = " + item.itemID, function (err, response) {
-                        if (err) throw err;
-                        // console.log(response);
-                        // console.log(parseInt(item.quantity));
-                        // console.log(res[0].stock_quantity);
-
-                        // If there is not enough stock, stop purchase request and ask user to make another purchase
-                        if (parseInt(item.quantity) > response[0].stock_quantity) {
-                            console.log("\nSorry, not enough in stock!\n");
-                            askUser();
-                        } else {
-                            // Updating the table in mysql to reflect new stock quantity after purchase
-                            connection.query(`UPDATE products SET stock_quantity = ${response[0].stock_quantity - parseInt(item.quantity)} WHERE item_id = ${item.itemID}`, function (err, res) {
-                                if (err) throw err;
-                                // console.log(res);
-                                console.log(`Transaction cost: $ ${response[0].price * parseInt(item.quantity)}`);
-                                askUser();
-                            });
-                        };
-                    });
-                });
+                makePurchase();
             }
             else if (answer.purchase === "Exit") {
                 console.log(`\nSee ya next time!\n`);
@@ -97,4 +58,46 @@ function askUser() {
             };
         });
     }, 500);
+};
+
+function makePurchase() {
+    inquirer.prompt([
+        {
+            name: "itemID",
+            type: "input",
+            message: "Enter the ID for the item you want to buy:"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many units of the item do you want to buy?"
+        }
+    ]).then(function (item) {
+        // Making the purchase request
+        console.log(`\nThe ID for the item is: ${item.itemID}\nYou want to buy: ${item.quantity} unit(s)`);
+        connection.query("SELECT * FROM products WHERE item_id = " + item.itemID, function (err, response) {
+            if (err) throw err;
+            // console.log(response);
+            // console.log(parseInt(item.quantity));
+            // console.log(res[0].stock_quantity);
+
+            // If there is not enough stock, stop purchase request and ask user to make another purchase
+            if (parseInt(item.quantity) > response[0].stock_quantity) {
+                console.log("\nSorry, not enough in stock!\n");
+                askUser();
+            } else {
+                // Updating the table in mysql to reflect new stock quantity after purchase
+                updateStock(response, item);
+            };
+        });
+    });
+};
+
+function updateStock(response, item) {
+    connection.query(`UPDATE products SET stock_quantity = ${response[0].stock_quantity - parseInt(item.quantity)} WHERE item_id = ${item.itemID}`, function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        console.log(`Transaction cost: $ ${response[0].price * parseInt(item.quantity)}`);
+        askUser();
+    });
 };
